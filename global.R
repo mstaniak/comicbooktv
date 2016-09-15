31821d4c97b5e8faf1a1a6168158170d08346459
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-09-16 00:32:44 +0200

    default values for new inputs

--- global.R
+++ global.R
@@ -1,10 +1,6 @@
 library(shiny)
 library(devtools)
 library(ggvis)
-# install_github("mstaniak/comicbookshows", dependencies = F)
-# if(!require(ToolsForTVShowsApp)) {
-# 	install_github("mstaniak/comicbookshows")
-# }
 library(ToolsForTVShowsApp)
 library(dplyr)
 library(tidyr)
@@ -21,6 +17,9 @@ episodes %>%
 defaultMinDate <- min(episodes$airDate) 
 defaultMaxDate <- max(episodes$airDate)
 defaultSeasons <- max(episodes$season)
+defaultSliderLeft <- min(episodes$imdbRating)
+defaultSliderRight <- max(episodes$imdbRating)
+defaultSeasonsNumber <- max(episodes$season[episodes$showTitle == names(showsTitles)[1]])
 
 leftWidth <- 3
 rightWidth <- 12 - leftWidth
