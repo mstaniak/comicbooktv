26183a3fb0d6ff8050e6f36018fe542803307307
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-09-16 16:45:38 +0200

    outputs draft

--- server.R
+++ server.R
@@ -1,4 +1,37 @@
 shinyServer(function(input, output, session) {
-    # output$imdbPlotOne
-  # output$imdbTextOne <-
+  firstName <- reactive({
+    names(showsTitles)[showsTitles == input$firstShow]
+  })
+  secondName <- reactive({
+    names(showsTitles)[showsTitles == input$secondShow]
+  }) 
+
+#   firstEpCounter <- reactive({
+#     episodes %>%
+#       filter(showTitle == firstName(),
+# 	     )
+#   })
+
+  output$oneShowPlot  <- renderPlot({
+    plotRatings(showName = firstName(),
+		typeRating = input$typeRating) 
+  })
+  output$compareShowsPlot <- renderPlot({
+    plotRatings(showName = c(firstName(), secondName()),
+		typeRating = input$typeRating )
+  })
+  output$oneShowInfo <- renderText({
+    if(is.null(input$plotOneClick)) {
+      return("")
+    } else {
+        paste(input$plotOneClick$x, input$plotOneClick$y)	    
+      }  
+  })
+  output$compareInfo1 <- renderText({
+    paste(firstName(), "has", "lol",
+	  "episodes in this time and ratings  range.")
+  })
+  output$compareInfo2 <- renderText({
+    paste0("lalala", "twoja stara")
+  })
 })
