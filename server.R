08ad0b280080e084e210f45bec98171ddbc26bf0
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-09-17 00:59:17 +0200

    filtering only in one place

--- server.R
+++ server.R
@@ -1,25 +1,42 @@
 shinyServer(function(input, output, session) {
+  
+  filteredData <- reactive({
+    filterToPlot(showNames = c(firstName(), secondName()),
+		 typeRating = input$typeRating,
+		 seasons = input$seasons,
+		 minRating = input$rt[1],
+		 maxRating = input$rt[2],
+		 minDate = input$dates[1],
+		 maxDate = input$dates[2])
+  })
+
   firstName <- reactive({
     names(showsTitles)[showsTitles == input$firstShow]
   })
+
   secondName <- reactive({
     names(showsTitles)[showsTitles == input$secondShow]
   }) 
 
-#   firstEpCounter <- reactive({
-#     episodes %>%
-#       filter(showTitle == firstName(),
-# 	     )
-#   })
+  firstEpCounter <- reactive({
+    filteredData() %>%
+      filter(showTitle == firstName()) %>%
+      n_distinct()
+  })
+  secondEpCounter <- reactive({
+    filteredData() %>%
+      filter(showTitle == secondName()) %>%
+      n_distinct()
+  })
 
   output$oneShowPlot  <- renderPlot({
-    plotRatings(showName = firstName(),
-		typeRating = input$typeRating) 
+    plotRatings(filteredData) 
   })
+
   output$compareShowsPlot <- renderPlot({
-    plotRatings(showName = c(firstName(), secondName()),
-		typeRating = input$typeRating )
+    plotRatings(filteredData)
   })
+
   output$oneShowInfo <- renderText({
     if(is.null(input$plotOneClick)) {
       return("")
@@ -27,11 +44,18 @@ shinyServer(function(input, output, session) {
         paste(input$plotOneClick$x, input$plotOneClick$y)	    
       }  
   })
-  output$compareInfo1 <- renderText({
-    paste(firstName(), "has", "lol",
-	  "episodes in this time and ratings  range.")
+
+  output$compareTop <- renderText({
+    paste(firstName(), "has", firstEpCounter(),
+	  "episodes in this range.")
   })
-  output$compareInfo2 <- renderText({
-    paste0("lalala", "twoja stara")
+
+  output$compareMid <- renderText({
+    paste(secondName(), "has", secondEpCounter(),
+	  "episodes in this range.")
+  })
+
+  output$compareBot <- renderText({
+    "Episode details:"
   })
 })
