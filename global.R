ca119ced5830976dbf9fddf2e96b6bcebbdd06fb
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-09-17 16:09:08 +0200

    inputs update added, ui improved

--- global.R
+++ global.R
@@ -10,20 +10,27 @@ library(ggplot2)
 library(markdown)
 library(lazyeval)
 
-showsTitles <- c("aca", "aos", "arr", "con", "dde", "fla", "got", "izo", "jjo", "lot", "luc", "pre", "sgi")
-episodes %>%
+showsTitles <- c("aca", "aos", "arr", "con", "fla", "got", "izo", "lot", "luc", "pre", "sgi")
+episodesPlus  %>%
+  filter(channel != "Netflix") %>%
   select(showTitle) %>%
   distinct() %>%
-  unlist(use.names = FALSE) -> names(showsTitles)
+  unlist(use.names = FALSE) %>%
+  sort() -> names(showsTitles)
 
 defaultMinDate <- min(episodes$airDate) 
 defaultMaxDate <- max(episodes$airDate)
 defaultSeasons <- max(episodes$season)
 defaultSliderLeft <- min(episodes$imdbRating)
 defaultSliderRight <- max(episodes$imdbRating)
+defaultSliderNielsenLeft <- min(episodes$nielsenRating, na.rm = T)
+defaultSliderNielsenRight <- max(episodes$nielsenRating, na.rm = T)
 defaultSeasonsNumber <- max(episodes$season[episodes$showTitle == names(showsTitles)[1]])
+defaultSeasonsNumberTwo <- max(episodes$season[episodes$showTitle == names(showsTitles[2])])
 defaultSeasonsChoices <- as.character(1:defaultSeasonsNumber)
 names(defaultSeasonsChoices) <- as.numeric(defaultSeasonsChoices)
+defaultSeasonsChoicesTwo <- as.character(1:defaultSeasonsNumberTwo)
+names(defaultSeasonsChoicesTwo) <- as.numeric(defaultSeasonsChoicesTwo)
 
 leftWidth <- 2
 rightWidth <- 12 - leftWidth
--- server.R
+++ server.R
@@ -1,27 +1,29 @@
 shinyServer(function(input, output, session) {
-  
-  filteredData <- reactive({
-    if(input$tabPanels == "oneShow") {
-      chosenNames <- firstName()
-    } else {
-      chosenNames <- c(firstName(), secondName())
-    } 
-    filterToPlot(showNames = chosenNames,
-		 typeRating = input$typeRating,
-		 seasons = input$seasons,
-		 minRating = input$rt[1],
-		 maxRating = input$rt[2],
-		 minDate = input$dates[1],
-		 maxDate = input$dates[2])
-  })
-
   firstName <- reactive({
     names(showsTitles)[showsTitles == input$firstShow]
   })
 
   secondName <- reactive({
-    names(showsTitles)[showsTitles == input$secondShow]
+    if(input$tabPanels == "compareShows") {
+      names(showsTitles)[showsTitles == input$secondShow]
+    } else if(input$tabPanels == "oneShow") {
+      firstName()
+    }
   }) 
+  
+  filteredData <- reactive({
+    seasonsList <- list(input$firstSeasons, input$secondSeasons)
+    if(input$tabPanels == "oneShow") {
+      seasonsList <- list(input$firstSeasons, input$firstSeasons)
+    }
+    filterToPlot(showNames = c(firstName(), secondName()),
+		 typeRating = input$typeRating,
+		 seasons = seasonsList)
+		 minRating = input$rt[1],
+		 maxRating = input$rt[2],
+		 minDate = input$dates[1],
+		 maxDate = input$dates[2])
+  })
 
   firstEpCounter <- reactive({
     filteredData()[[2]] %>%
@@ -34,12 +36,55 @@ shinyServer(function(input, output, session) {
       n_distinct()
   })
 
+  firstMaxSeason <- reactive({
+    shows %>%
+      filter(showTitle == firstName()) %>%
+      summarise(maxS = max(season, na.rm = TRUE)) %>%
+      select(maxS) %>%
+      unlist(use.names = FALSE) 
+  })
+  secondMaxSeason <- reactive({
+    shows %>% 
+      filter(showTitle == secondName()) %>%
+      summarise(maxS = max(season, na.rm = TRUE)) %>%
+      select(maxS) %>%
+      unlist(use.names = FALSE)
+  })
+
+  observe({
+    if(input$typeRating == "nielsenRating") {
+      updateSliderInput(session, "rt",
+			min = floor(defaultSliderNielsenLeft),
+			max = ceiling(defaultSliderNielsenRight),
+			value = c(defaultSliderNielsenLeft, defaultSliderNielsenRight))
+    } else {
+      updateSliderInput(session, "rt",       
+			min = floor(defaultSliderLeft),
+			max = ceiling(defaultSliderRight),
+			value = c(defaultSliderLeft,
+				  defaultSliderRight))
+    }
+
+    seasonsChoices <- as.character(1:firstMaxSeason())
+    names(seasonsChoices) <- as.numeric(seasonsChoices)
+    updateCheckboxGroupInput(session, "firstSeasons",
+			     choices = seasonsChoices,
+			     selected = seasonsChoices,
+			     inline = TRUE)
+    seasonsChoicesTwo <- as.character(1:secondMaxSeason())
+    names(seasonsChoicesTwo) <- as.numeric(seasonsChoicesTwo)
+    updateCheckboxGroupInput(session, "secondSeasons",
+			     choices = seasonsChoicesTwo,
+			     selected = seasonsChoicesTwo,
+			     inline = TRUE)
+  })
+
   output$oneShowPlot  <- renderPlot({
-    plotRatings(filteredData()) 
+    plotRatings(filteredData(), trend = input$trend) 
   })
 
   output$compareShowsPlot <- renderPlot({
-    plotRatings(filteredData())
+    plotRatings(filteredData(), trend = input$trend)
   })
 
   output$oneShowInfo <- renderText({
@@ -61,10 +106,10 @@ shinyServer(function(input, output, session) {
   })
 
   output$oneShowInfo <- renderText({
-    tooltip(filteredData()[[2]], input$plotOneClick, input$typeRating)
+   giveTooltip(filteredData()[[2]], input$plotOneClick, input$typeRating)
   })
 
   output$compareBot <- renderText({
-    tooltip(filteredData()[[2]], input$plotTwoClick, input$typeRating)
+    giveTooltip(filteredData()[[2]], input$plotTwoClick, input$typeRating)
   })
 })
